bc29db14b52337b5c1f2a4f6be745075cc31b4c5
Martin Becker <mbeckerwork@gmail.com>
2017-07-25 08:40:17 +0100

    adds npm aliases

--- .zsh/custom/aliases.sh
+++ .zsh/custom/aliases.sh
@@ -7,3 +7,6 @@ alias tmux="TERM=screen-256color-bce tmux -2"
 alias gitdelbranch=gitdelbranchfunc
 
 alias ctagsforgems="ctags -R -f gems.tag $(bundle show --paths)"
+
+alias ng="npm list -g --depth=0 2>/dev/null"
+alias nl="npm list --depth=0 2>/dev/null"
